0df30256529ba123053920ddc60b342b1baf393c
Raysa Gomes <raysagomes2009@gmail.com>
2025-09-09 19:53:59 -0300

    criado o projeto

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Bem-vindo à página Home</h1>
+      <p>Esta é a rota /home.</p>
+      </div>
+  );
+}
